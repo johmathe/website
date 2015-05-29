5f98e06f380843478cd899b060ab225b3da08734
Johan Mathe <johan.mathe@gmail.com>
2015-05-29 19:20:54 +0300

    new site layout

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personal page doc
+
+git 
---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Johan Mathe'
+SITENAME = u'Johan Mathe'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = False
+
+# Social widget
+SOCIAL = False
+
+DEFAULT_PAGINATION = 10
+
+THEME = "pelican-themes/subtle"
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
